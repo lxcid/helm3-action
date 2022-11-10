b51863711e7fc8f0b28a52fa070fb37608f98ae2
Stan Chang Khin Boon <khinboon@gmail.com>
2022-11-11 00:44:33 +0800

    fix: Make kube config data optional

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-# Extract the base64 encoded config data and write this to the KUBECONFIG
-echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
-export KUBECONFIG=/tmp/config
+if [[ ! -z ${KUBE_CONFIG_DATA+x} ]]; then
+  # Extract the base64 encoded config data and write this to the KUBECONFIG
+  echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
+  export KUBECONFIG=/tmp/config
+  echo "Set KUBECONFIG to /tmp/config"
+fi
 
 sh -c "helm $*"
